79fd4f9d84ad81228ce9346840c9c61ec7c940ae
sopan1993 <46264317+sopan1993@users.noreply.github.com>
2019-06-06 12:14:51 +0530

    Update ViewController.swift

--- WebserviceDemo/ViewController.swift
+++ WebserviceDemo/ViewController.swift
@@ -226,6 +226,22 @@ class ViewController: UIViewController {
  
 }
  */
+extension UIImageView {
+    public func imageFromServerURL(urlString: String) {
+        self.image = nil
+        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
+
+            if error != nil {
+                print(error)
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.image = image
+            })
+
+        }).resume()
+    }}
 
 
 
